03fe2363ed8f4f745f05a2d81a66347e1f49aadd
Ihtasham <redrailz@gmail.com>
2016-11-06 17:35:15 +0000

    Create server.py

---
+++ server/server.py
@@ -0,0 +1,31 @@
+import pusher
+from yandex_translate import YandexTranslate
+
+
+pusher_client = pusher.Pusher(
+  app_id='266740',
+  key='3fcff710e9cb5d9c3fb8',
+  secret='7f7f90d0cd5e279e32bb',
+  cluster='eu',
+  ssl=True
+)
+
+yandex_api = "" # yandex translate API key here
+
+translate = YandexTranslate(yandex_api)
+
+from_lang = raw_input("Select the source language (e.g. en): ")
+to_lang = raw_input("Select the target language (e.g. fr): ")
+
+gen = from_lang.lower()+"-"+to_lang.lower()
+
+print "Set source lang: {} | target lang: {}".format(from_lang, to_lang)
+
+while True:
+	text = raw_input("Enter a phrase to translate: ")
+	if text == "stop":
+		break
+	translated = translate.translate(text, gen)
+	pusher_client.trigger('test_channel', 'my_event', {'message': translated.get("text")[0]})
+
+print "Session complete."
